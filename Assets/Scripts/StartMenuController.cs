befa5006968e66bd15b69e4a86b3b29077853bab
Island <9435620+island212@users.noreply.github.com>
2025-11-24 18:28:30 -0500

    Fix issue for WebGL build

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -2,19 +2,6 @@ using UnityEngine;
 
 public class StartMenuController : MonoBehaviour
 {
-    [Header("UI References")]
-    public GameObject exitButton;
-
-    private void Awake()
-    {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        if (quitButton != null)
-        {
-            quitButton.gameObject.SetActive(false);
-        }
-#endif
-    }
-    
     public void OnStartClicked()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(SceneConstants.PLAYGROUND);
